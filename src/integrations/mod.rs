6064ef636078bc31d085b32b76d9fdfba8fd607f
xuesi <xuesiyang@lilith.com>
2024-11-05 15:29:01 +0800

    feat: support tvos

--- src/integrations/mod.rs
+++ src/integrations/mod.rs
@@ -1,6 +1,6 @@
 #[cfg(any(target_os = "linux", target_os = "android"))]
 mod linux;
-#[cfg(any(target_os = "macos", target_os = "ios"))]
+#[cfg(any(target_os = "macos", target_os = "ios", target_os = "tvos"))]
 mod osx;
 #[cfg(target_os = "windows")]
 mod windows;
@@ -10,7 +10,7 @@ pub use self::shared_api::*;
 
 #[cfg(any(target_os = "linux", target_os = "android"))]
 pub use self::linux::*;
-#[cfg(any(target_os = "macos", target_os = "ios"))]
+#[cfg(any(target_os = "macos", target_os = "ios", target_os = "tvos"))]
 pub use self::osx::*;
 #[cfg(target_os = "windows")]
 pub use self::windows::*;
